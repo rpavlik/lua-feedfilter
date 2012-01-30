46c644087ca89844d1ddca9b0402b369a350c48c
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-30 15:34:28 -0600

    Cache the downloaded rss file locally.

--- feedfilter/feed.lua
+++ feedfilter/feed.lua
@@ -4,13 +4,16 @@ local feedproto = {}
 
 feedproto.frequency = 60
 feedproto.get = function(self)
-	assert(self.url ~= nil, "URL must be provided!")
-	local https = require("feedfilter.https")
-	local b, c, h = https.request(self.url)
-	if c < 200 or c > 299 then
-		error(("Failed getting %s - code %d"):format(self.url, c))
+	if self.cachedBody == nil then
+		assert(self.url ~= nil, "URL must be provided!")
+		local https = require("feedfilter.https")
+		local b, c, h = https.request(self.url)
+		if c < 200 or c > 299 then
+			error(("Failed getting %s - code %d"):format(self.url, c))
+		end
+		self.cachedBody = b
 	end
-	return feedparser.parse(b)
+	return feedparser.parse(self.cachedBody)
 end
 
 local feedmt = { __index = feedproto }
